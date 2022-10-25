5abfdf4365fbb38b5fc9f01f38e9cb77294dfb41
janita <janitariyaz124@gmail.com>
2022-10-25 18:51:24 +0500

    comments in python

---
+++ Comments.py
@@ -0,0 +1,16 @@
+# comments are the thoes lines that are unexecutable. 
+# They are used to povise additional and useful inforamyion about code.
+
+
+
+ 
+# -> ---single line comments---: 
+#    single line comments is done by using #
+        
+    #This is single line comment
+
+#->  ---- Multiple line comments:---
+
+'''this 
+   is  a multi
+lines of comment'''
