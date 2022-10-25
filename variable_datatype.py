07690daf8219e597b9106ce5ef447773ef80c467
janita <janitariyaz124@gmail.com>
2022-10-25 19:21:57 +0500

    Variables and datatypes

---
+++ variable_datatype.py
@@ -0,0 +1,51 @@
+
+# variables:
+#       variables is the name given to memory location in a Program.
+        
+#     in simple words variable are containers that store some values.
+#     like Jar is used to store pickle.
+
+# for example:
+a='jnaita'
+b=123
+c=True
+
+#    ------Dtatypes----
+
+# there are folloeing types of data:
+
+'''
+-> integer:
+     integer are real numbers.
+     1,45,89
+
+-> Flotaing pont numbers:
+      floating type numbers are decimal number.
+      23.45, 56.67
+
+-> string:
+     string is the collection of characters.It used to wnclose in "" quotes.
+     Double or single quotes can use.
+
+     'janita', "John"
+
+->Boolean:
+   Boolean are the statements that can be TRUE and FALSE.
+'''
+
+
+
+'''
+   ***--------------------------------------------***
+   - Some POINTS TO REMEMBER:                       -
+   - Varible:                                       -                       
+   -           containers to store information.     -                                   
+   -  Keywords:                                     -                      
+   -          some reserve words in python          -                                     
+   -  Identifiers:                                  -                      
+   -       class/function/varable name              -                                  
+   ***--------------------------------------------***
+
+'''
+            
+
